dc544841cec0b8fbda6f43219a2f2142f3aeb3ae
AggerFivem <asgermunkdall10@gmail.com>
2024-12-05 15:13:17 +0100

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -82,7 +82,7 @@ SetupfirmabilRadial = function()
 end
 
 
-RegisterKeyMapping('openfirmabilRadial', 'Åbn firmabil Menu', 'keyboard', 'F1')
+
 RegisterCommand('openfirmabilRadial', function()
     if isInfirmabil then
         lib.showRadial('firmabil_menu')
@@ -111,29 +111,7 @@ function spawnFirmaBil()
 end
 
 
-function DrawText3D(x, y, z, text)
-    local onScreen, _x, _y = World3dToScreen2d(x, y, z)
-    local camCoords = GetGameplayCamCoords()
-    local distance = #(camCoords - vector3(x, y, z))
-    local scale = (1 / distance) * 2
-    local fov = (1 / GetGameplayCamFov()) * 100
-    scale = scale * fov
 
-    if onScreen then
-        SetTextScale(0.0 * scale, 0.35 * scale)
-        SetTextFont(4)
-        SetTextProportional(1)
-        SetTextColour(255, 255, 255, 215)
-        SetTextDropshadow(0, 0, 0, 0, 255)
-        SetTextEdge(2, 0, 0, 0, 150)
-        SetTextDropShadow()
-        SetTextOutline()
-        SetTextEntry("STRING")
-        SetTextCentre(1)
-        AddTextComponentString(text)
-        DrawText(_x, _y)
-    end
-end
 
 
 exports.ox_target:addGlobalVehicle(
